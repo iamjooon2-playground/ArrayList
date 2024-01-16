92aa95e41ec93d046d2cfc7709681cf03c77107c
iamjooon2 <iamjooon2@gmail.com>
2024-01-16 17:29:38 +0900

    feat: LinkedList 구현

--- src/main/java/arraylist/SimpleLinkedList.java
+++ src/main/java/arraylist/SimpleLinkedList.java
@@ -1,59 +1,158 @@
 package arraylist;
 
-public class SimpleLinkedList implements SimpleList{
+public class SimpleLinkedList implements SimpleList {
+
+    private Node head;
+    private int size;
+
+    public SimpleLinkedList() {
+        head = new Node();
+        size = 0;
+    }
+
+    static class Node {
+
+        private String value;
+        private Node next;
+
+        public Node() {
+        }
+
+        public Node(String value) {
+            this.value = value;
+        }
+    }
 
     @Override
     public boolean add(String value) {
-        return false;
+        Node node = head;
+        while (node.next != null) {
+            node = node.next;
+        }
+
+        node.next = new Node(value);
+        size++;
+        return true;
     }
 
     @Override
     public void add(int index, String value) {
+        validateIndexRange(index);
 
+        Node before = getBeforeNodeBy(index);
+        Node after = before.next;
+
+        Node newNode = new Node(value);
+        before.next = newNode;
+        newNode.next = after;
+
+        size++;
+    }
+
+    private Node getBeforeNodeBy(int index) {
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    private void validateIndexRange(int index) {
+        if (index < 0 || index >= size) {
+            throw new RuntimeException("인덱스 범위가 잘못됐어용");
+        }
     }
 
     @Override
     public String set(int index, String value) {
-        return null;
+        validateIndexRange(index);
+
+        Node now = getNodeBy(index);
+        String beforeValue = now.value;
+
+        now.value = value;
+        return beforeValue;
+    }
+
+    private Node getNodeBy(int index) {
+        Node node = head;
+        for (int i = 0; i <= index; i++) {
+            node = node.next;
+        }
+        return node;
     }
 
     @Override
     public String get(int index) {
-        return null;
+        validateIndexRange(index);
+
+        Node now = getNodeBy(index);
+        return now.value;
     }
 
     @Override
     public boolean contains(String value) {
+        Node node = head;
+        while (node.next != null) {
+            node = node.next;
+            if (node.value.equals(value)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public int indexOf(String value) {
-        return 0;
+        Node node = head;
+        for (int i = 0; i < size; i++) {
+            node = node.next;
+            if (value.equals(node.value)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean remove(String value) {
+        Node node = head;
+        while (node.next != null) {
+            if (value.equals(node.next.value)) {
+                node.next = node.next.next;
+                size--;
+                return true;
+            }
+            node = node.next;
+        }
         return false;
     }
 
     @Override
     public String remove(int index) {
-        return null;
+        validateIndexRange(index);
+
+        Node node = getBeforeNodeBy(index);
+        String target = node.next.value;
+        node.next = node.next.next;
+
+        size--;
+        return target;
     }
 
     @Override
     public void clear() {
-
+        head = new Node();
+        size = 0;
     }
 }
--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -13,21 +13,18 @@ class LinkedListTest {
 
         values.add("first");
         values.add("second");
-        System.out.println("values = " + values);
         assertThat(values.indexOf("second")).isEqualTo(1);
         assertThat(values.add("third")).isTrue(); // 세 번째 값을 추가한다.
         assertThat(values.contains("third")).isTrue();
         assertThat(values.indexOf("third")).isEqualTo(2);
         values.set(0, "1");
         assertThat(values.contains("1")).isTrue();
-        System.out.println("values = " + values);
         values.set(0, "first");
-        System.out.println("values = " + values);
         assertThat(values.size()).isEqualTo(3); // list의 크기를 구한다.
         assertThat(values.get(0)).isEqualTo("first"); // 첫 번째 값을 찾는다.
-        assertThat(values.get(1)).isEqualTo("second"); // 첫 번째 값을 찾는다.
-        assertThat(values.get(2)).isEqualTo("third"); // 첫 번째 값을 찾는다.
-        assertThatThrownBy(() -> values.get(3)).isInstanceOf(IndexOutOfBoundsException.class);
+        assertThat(values.get(1)).isEqualTo("second"); // 두 번째 값을 찾는다.
+        assertThat(values.get(2)).isEqualTo("third"); // 세 번째 값을 찾는다.
+        assertThatThrownBy(() -> values.get(3)).isInstanceOf(RuntimeException.class);
         assertThat(values.contains("first")).isTrue(); // "first" 값이 포함되어 있는지를 확인한다.
         assertThat(values.contains("forth")).isFalse(); // "first" 값이 포함되어 있는지를 확인한다.
         assertThat(values.remove(0)).isEqualTo("first"); // 첫 번째 값을 삭제한다.
@@ -37,11 +34,8 @@ class LinkedListTest {
         assertThat(values.remove(1)).isEqualTo("third"); // 첫 번째 값을 삭제한다.
         assertThat(values.remove(0)).isEqualTo("second"); // 첫 번째 값을 삭제한다.
         assertThat(values.add("four")).isTrue();
-        System.out.println(values);
         assertThat(values.get(0)).isEqualTo("four");
-        System.out.println(values);
         values.add(0, "five");
-        System.out.println(values);
         assertThat(values.get(0)).isEqualTo("five");
         assertThat(values.get(1)).isEqualTo("four");
         assertThat(values.contains("five")).isTrue();
@@ -49,9 +43,7 @@ class LinkedListTest {
         assertThat(values.indexOf("five")).isEqualTo(0);
         assertThat(values.remove("five")).isTrue();
         assertThat(values.get(0)).isEqualTo("four");
-        System.out.println("values = " + values);
         assertThat(values.set(0, "five")).isEqualTo("four");
-        System.out.println(values);
         assertThat(values.get(0)).isEqualTo("five");
         values.clear();
         assertThat(values.size()).isEqualTo(0);
